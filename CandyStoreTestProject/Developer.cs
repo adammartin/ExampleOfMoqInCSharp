4e777d27524c5b9974eb3d78695938c7d35c9807
Adam Martin <yance.martin@gmail.com>
2013-11-19 15:11:13 -0600

    Implemented the Moq example

---
+++ CandyStoreTestProject/Developer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Candy;
+
+namespace CandyStoreTestProject
+{
+    class Developer
+    {
+        public void BuyTastiestCandy(ICandyShop candyShop)
+        {
+            candyShop.BuyMostPopularCandy();
+        }
+    }
+}
--- CandyStoreTestProject/DeveloperTest.cs
+++ CandyStoreTestProject/DeveloperTest.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using Moq;
 using Xunit;
+using Candy;
 
 namespace CandyStoreTestProject
 {
     public class DeveloperTest
     {
         [Fact]
-        public void DeveloperWillBuyCandy()
+        public void DeveloperWillBuyCandyFromTheCandyStore()
         {
+            var mockCandyShop = new Mock<ICandyShop>();
+            RawCandy expectedCandy;
+            expectedCandy.Type = "bar";
+            expectedCandy.Brand = "foo";
+            mockCandyShop.Setup(aCandyShop => aCandyShop.BuyMostPopularCandy()).Returns(expectedCandy).Verifiable();
+
+            var aDeveloper = new Developer();
+            aDeveloper.BuyTastiestCandy(mockCandyShop.Object);
+            mockCandyShop.Verify();
         }
     }
 }
