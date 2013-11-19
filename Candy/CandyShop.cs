47c94992700b333501c61a3abe290457a1b8aecc
Adam Martin <yance.martin@gmail.com>
2013-11-19 14:44:03 -0600

    Setting up testing scenerio

---
+++ Candy/CandyShop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candy
+{
+    public struct RawCandy
+    {
+        public string Brand;
+        public string Type;
+    }
+
+    public interface ICandyShop
+    {
+        RawCandy BuyMostPopularCandy();
+    }
+
+    public class CandyShop : ICandyShop
+    {
+        public RawCandy BuyMostPopularCandy()
+        {
+            throw new NotImplementedException("You should be using the Mock silly!");
+        }
+    }
+}
